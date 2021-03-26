8c9e76a0e2e4834d6bb74e342945cafb62ed9b43
lastnameswayne <tiarnan1109@gmail.com>
2021-03-26 10:09:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# startergraphqlserver
+
+This is a starter server for some of my blogposts. Just clone this, and you will definitely be able to follow the article✅
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "type-graphql";
+import { getConnectionOptions, createConnection } from "typeorm";
+import { HelloWorldResolver } from "./resolvers/HelloWorldResolver";
+
+(async () => {
+  const app = express();
+
+  const options = await getConnectionOptions(
+    process.env.NODE_ENV || "development"
+  );
+  await createConnection({ ...options, name: "default" });
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloWorldResolver],
+      validate: true,
+    }),
+    context: ({ req, res }) => ({ req, res }),
+  });
+
+  apolloServer.applyMiddleware({ app, cors: false });
+  const port = process.env.PORT || 4000;
+  app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}/graphql`);
+  });
+})();
